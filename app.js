3b06d6863aa3badf0f3e0d39a46842dc489722ef
MadMonkey87 <myself@philippewechsler.ch>
2020-12-29 16:27:13 +0100

    basic flow cards

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ const Homey = require('homey');
 class SplinesApp extends Homey.App {
   async onInit() {
     this.log('SplinesApp has been initialized');
+
+    let queryCompletedAction = new Homey.FlowCardTrigger('query_completed').register();
+
+    let querySplineAction = new Homey.FlowCardAction('query_spline');
+    querySplineAction
+      .register()
+      .registerRunListener(async (args, state) => {
+        return new Promise((resolve) => {
+          const tokens = { result: args.value };
+          this.log('query completed ', tokens);
+          queryCompletedAction.trigger(tokens);
+
+          resolve(true);
+        });
+      });
+
+    querySplineAction.getArgument('spline')
+      .registerAutocompleteListener((query, args) => {
+        return new Promise((resolve) => {
+          let splines = Homey.ManagerSettings.get('splines');
+          if (splines == undefined || splines === null) {
+            splines = [];
+          }
+          resolve(splines);
+        });
+      });
   }
 }
 
